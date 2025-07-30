088ae2dc159980b091573b7f05f0955ad0b4d09c
ParagNirwan <pnirwan9@gmail.com>
2025-07-30 18:09:05 +0200

    Configurations Done

---
+++ apps/ecom-backend/src/main/java/com/petcommerce/ecom/PetCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.petcommerce.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCommerceApplication.class, args);
+	}
+
+}
---
+++ apps/ecom-backend/src/main/java/com/petcommerce/ecom/wire/security/primary/CorsFilterConfiguration.java
@@ -0,0 +1,28 @@
+package com.petcommerce.ecom.wire.security.primary;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsFilterConfiguration {
+  private CorsConfiguration corsConfiguration;
+
+  public void CorsConfiguration(CorsConfiguration corsConfiguration){
+    this.corsConfiguration = corsConfiguration;
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter(){
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(/*source*/));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
+  }
+
+}
